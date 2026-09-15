9d032ac7aa1a296745896b725ac347ee7e58e1a8
LEETRONGDAT <109588680+LEETRONGDAT@users.noreply.github.com>
2026-09-15 15:36:04 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const CACHE='l100-v3-profile';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./data.js','./manifest.webmanifest','./icon.svg']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const copy=x.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return x}))));
+const CACHE='l100-v4';
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon.svg'])))});
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const copy=x.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return x})));});
